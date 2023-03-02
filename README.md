f78ff73c0d55579a0c4f8561aac039f1785291f0
Pedro Alves <pta2002@pta2002.com>
2023-03-02 01:23:30 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Home automation
+Some python code to control my home automation. It is absolutely not portable! If you wish to use it, you'll have to change the code.
---
+++ main.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+import json
+
+switches = ["zigbee2mqtt/0xcc86ecfffe87753d", "zigbee2mqtt/0x50325ffffe784b22"]
+ceil_light = "zigbee2mqtt/0x70ac08fffeb95221"
+lights = [
+    ceil_light,
+    "zigbee2mqtt/0xb4e3f9fffeb34960",
+    "zigbee2mqtt/0xcc86ecfffe8d40c7",
+]
+
+# TODO: Publish current light as a home assistant sensor
+# https://github.com/hjelev/rpi-mqtt-monitor/blob/master/src/rpi-cpu2mqtt.py
+
+# TODO: Allow brightness up/down
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    for switch in switches:
+        client.subscribe(switch)
+
+
+def on_message(client, userdata, msg):
+    cur_light = userdata["cur_light"]
+    print(f"Current light: {cur_light}")
+    light = lights[cur_light]
+    if msg.topic == switches[1]:
+        light = ceil_light
+
+    if msg.topic in switches:
+        payload = json.loads(msg.payload)
+        match payload["action"]:
+            case "on":
+                client.publish(f"{light}/set", json.dumps({"state": "ON"}))
+            case "off":
+                client.publish(f"{light}/set", json.dumps({"state": "OFF"}))
+            case "arrow_right_click":
+                client.user_data_set({"cur_light": (cur_light + 1) % len(lights)})
+            case "arrow_left_click":
+                cur_light = (cur_light - 1 + len(lights)) % len(lights)
+                client.user_data_set({"cur_light": cur_light})
+
+
+client = mqtt.Client(userdata={"cur_light": 0})
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("pie", 1883, 60)
+
+client.loop_forever()
