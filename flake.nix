db0df3935ca24fb4b3c1d88dfdda503f7f27569c
Pedro Alves <pta2002@pta2002.com>
2023-03-02 01:28:48 +0000

    add package to nix flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,21 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
+      packages.default = pkgs.stdenv.mkDerivation rec {
+        name = "my-switches";
+        src = ./.;
+        propagatedBuildInputs = with pkgs; [
+          (python3.withPackages (p: with p; [
+            paho-mqtt
+          ]))
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ./main.py $out/bin/my-switches
+        '';
+      };
+
       devShell = pkgs.mkShell {
         buildInputs = [
           (pkgs.python3.withPackages (ps: with ps; [
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import paho.mqtt.client as mqtt
 import json
 
@@ -46,6 +47,6 @@ client = mqtt.Client(userdata={"cur_light": 0})
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("pie", 1883, 60)
+client.connect("localhost", 1883, 60)
 
 client.loop_forever()
