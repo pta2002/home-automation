557565f26794d64ce578e49d08efde9c117a5ce2
Pedro Alves <pta2002@pta2002.com>
2024-05-25 20:09:23 +0100

    change remote name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import paho.mqtt.client as mqtt
 import json
 
-switches = ["zigbee2mqtt/large_remote"]
+switches = ["zigbee2mqtt/big_remote"]
 lights = [
     "zigbee2mqtt/ceiling_light",
     "zigbee2mqtt/bed_light",
@@ -18,6 +18,7 @@ lights = [
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     for switch in switches:
+        print(f"connecting to switch {switch}")
         client.subscribe(switch)
 
 
@@ -26,23 +27,26 @@ def on_message(client, userdata, msg):
     print(f"Current light: {cur_light}")
     light = lights[cur_light]
 
+    print(f"Received message {msg.topic}: {msg.payload}")
+
     if msg.topic in switches:
         payload = json.loads(msg.payload)
-        match payload["action"]:
-            case "on":
-                client.publish(f"{light}/set", json.dumps({"state": "ON"}))
-            case "off":
-                client.publish(f"{light}/set", json.dumps({"state": "OFF"}))
-            case "arrow_right_click":
-                client.user_data_set({"cur_light": (cur_light + 1) % len(lights)})
-            case "arrow_left_click":
-                cur_light = (cur_light - 1 + len(lights)) % len(lights)
-                client.user_data_set({"cur_light": cur_light})
+
+        if payload["action"] == "on":
+            client.publish(f"{light}/set", json.dumps({"state": "ON"}))
+        elif payload["action"] == "off":
+            client.publish(f"{light}/set", json.dumps({"state": "OFF"}))
+        elif payload["action"] == "arrow_right_click":
+            client.user_data_set({"cur_light": (cur_light + 1) % len(lights)})
+        elif payload["action"] == "arrow_left_click":
+            cur_light = (cur_light - 1 + len(lights)) % len(lights)
+            client.user_data_set({"cur_light": cur_light})
 
 
 client = mqtt.Client(userdata={"cur_light": 0})
 client.on_connect = on_connect
 client.on_message = on_message
+client.enable_logger()
 
 client.connect("localhost", 1883, 60)
 
